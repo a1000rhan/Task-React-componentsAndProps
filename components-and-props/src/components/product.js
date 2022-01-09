584622109e07e079842d5f899e49ace24e9d21f0
a1000rhan <1000rhan@gmail.com>
2022-01-09 14:39:07 +0300

    done

--- components-and-props/src/components/product.js
+++ components-and-props/src/components/product.js
@@ -1,16 +1,12 @@
-import React from 'react'
-import Product from './productList'
-import productList from '../Products'
+import React from "react";
+import Product from "./productList";
+import productList from "../Products";
 
 export default function product() {
-    const productFile=productList.map(prod=>(<div className="container">
-        <img className="img" src={prod.image} />
-     <div> <p className="font">{prod.name}</p>
-        <p className="price">{prod.price} USD</p></div>
-    </div>))
-    return (
-        <div>
-           <div className="border"> {productFile}</div>
-        </div>
-    )
+  const productFile = productList.map((prod) => <Product list={prod} />);
+  return (
+    <div>
+      <div className="border"> {productFile}</div>
+    </div>
+  );
 }
--- components-and-props/src/components/productList.js
+++ components-and-props/src/components/productList.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import Products from '../Products';
+import React from "react";
+import Products from "../Products";
 
-
-export default function productList() {
-   
-    return (
-        <div>
-          
-        </div>
-    )
+export default function productList({ list }) {
+  return (
+    <div className="container">
+      <img className="img" src={list.image} />
+      <div>
+        {" "}
+        <p className="font">{list.name}</p>
+        <p className="price">{list.price} USD</p>
+      </div>
+    </div>
+  );
 }
